87fe7ff0ae300208fd9bcbd9d731d40177916d0e
mkmasud1 <mkmasud387@gmail.com>
2026-04-18 14:07:44 +0600

    Add files via upload

---
+++ asset/js/custom.js
@@ -0,0 +1,20 @@
+// my codeing start 
+const menuIcon = document.getElementById("menu_icon")
+const menu = document.getElementById("menu")
+menuIcon.addEventListener("click",()=>{
+    if(menu.className === "hidden"){
+        menu.classList.remove("hidden");
+    }else{menu.classList.add("hidden");}
+        
+    
+});
+
+// nav code start 
+
+// nav code end 
+
+
+// carosel code start 
+
+// carosel code end
+// my codeing end
